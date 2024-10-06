42f3003bfd1307c3b75c9f61314edb57759b078d
azaria amanda <azaryaamanda@gmail.com>
2024-10-06 14:39:35 +0700

    tugas jobsheet6

---
+++ daspro-jobsheet6/Pemilihan2Percobaan102.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/**
+ * Pemilihan2Percobaan102
+ */
+public class Pemilihan2Percobaan102 {
+
+    public static void main(String[] args) {
+        Scanner input02 = new Scanner(System.in);
+
+        System.out.print("Masukkan tahun: ");
+        int tahun = input02.nextInt();
+
+        if ((tahun % 4) == 0) {  
+            if ((tahun % 100) != 0)  
+                System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
