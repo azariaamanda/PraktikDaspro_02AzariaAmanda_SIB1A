e4000e4ffbec5c19032a8d6f2d2d3310895e4945
azaria amanda <azaryaamanda@gmail.com>
2024-10-06 16:07:09 +0700

    Modifikasi Percobaan 1

--- daspro-jobsheet6/Pemilihan2Percobaan102.java
+++ daspro-jobsheet6/Pemilihan2Percobaan102.java
@@ -10,9 +10,18 @@ public class Pemilihan2Percobaan102 {
         System.out.print("Masukkan tahun: ");
         int tahun = input02.nextInt();
 
-        if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
-            System.out.println("Tahun Kabisat");
-        } else 
+        if ((tahun % 4) == 0) {  
+            if ((tahun % 100) == 0) {  
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
